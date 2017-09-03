a22335ef3de6da8ce9d92a94a9fa11c810ae7eaf
KCHETTY <kchetty416@gmail.com>
2017-09-03 12:01:47 +0200

    a

--- CMakeFiles/Bomberman.dir/DependInfo.cmake
+++ CMakeFiles/Bomberman.dir/DependInfo.cmake
@@ -4,42 +4,38 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Camera.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Camera.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Component.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Component.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Data_Loader.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Data_Loader.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Light_class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Light_class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Model.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Model.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Model_Text.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Model_Text.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Object_Loader.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Object_Loader.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Render.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Render.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Render_Engine.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Render_Engine.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Shaders.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Shaders.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Graphics_lib/Src/Text_Model.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Text_Model.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/Sound_lib/src/SoundEngine.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/Sound_lib/src/SoundEngine.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/AEntity.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/AEntity.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Bomb.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Bomb.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Enemy.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Enemy.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Exception.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Exception.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Explosion.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Explosion.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/GameEngine.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/GameEngine.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Gate.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Gate.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Player.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Player.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Powerup.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Powerup.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/Wall.class.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/Wall.class.cpp.o"
-  "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/src/main.cpp" "/nfs/zfs-student-6/users/kchetty/Desktop/this_shit/CMakeFiles/Bomberman.dir/src/main.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Camera.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Camera.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Component.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Component.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Data_Loader.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Data_Loader.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Light_class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Light_class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Model.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Model.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Model_Text.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Model_Text.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Object_Loader.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Object_Loader.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Render.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Render.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Render_Engine.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Render_Engine.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Shaders.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Shaders.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Graphics_lib/Src/Text_Model.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Graphics_lib/Src/Text_Model.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/Sound_lib/src/SoundEngine.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/Sound_lib/src/SoundEngine.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/AEntity.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/AEntity.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Bomb.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Bomb.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Enemy.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Enemy.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Exception.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Exception.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Explosion.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Explosion.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/GameEngine.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/GameEngine.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Gate.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Gate.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Player.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Player.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Powerup.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Powerup.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/Wall.class.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/Wall.class.cpp.o"
+  "/home/kchetty/Desktop/SHEEEP/src/main.cpp" "/home/kchetty/Desktop/SHEEEP/CMakeFiles/Bomberman.dir/src/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "./include"
   "./Graphics_lib/Inc"
-  "/nfs/zfs-student-6/users/kchetty/.brew/include"
   "Graphics_lib/Soil/SOIL2"
-  "/System/Library/Frameworks/OpenAL.framework/Headers"
-  "/nfs/zfs-student-6/users/kchetty/.brew/opt/glew/include"
-  "libs/glm"
-  "/System/Library/Frameworks/OpenGL.framework/Headers"
+  "/usr/include/AL"
   )
 
 # Targets to which this target links.
--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /nfs/zfs-student-6/users/jbuys/Desktop/Projects/C++/Bomberman
+# Install script for directory: /home/kchetty/Desktop/SHEEEP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/nfs/zfs-student-6/users/jbuys/.brew")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -35,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/nfs/zfs-student-6/users/jbuys/Desktop/Projects/C++/Bomberman/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/kchetty/Desktop/SHEEEP/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
