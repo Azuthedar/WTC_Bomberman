676f6dc2868c0c7d079faad8c231d26a74b3352c
Anton RICHTER <antricht@e4r24p5.wtc.co.za>
2017-09-07 10:45:27 +0200

    Scripts

---
+++ brew_install.sh
@@ -0,0 +1,3 @@
+echo "Installing Brew, Please Be Patient..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
+reset
---
+++ install.sh
@@ -0,0 +1,14 @@
+echo "Installing CMake..."
+brew install cmake
+echo "Installing Assimp..."
+brew install assimp
+echo "Installing GLFW..."
+brew install glfw
+echo "Installing Glew..."
+brew install glew
+echo "Installing FreeAlut..."
+brew install freealut
+echo "Installing GLM..."
+brew install glm
+echo "Please Run The Following Command: cmake ."
+echo "Then Run The Following Command: make"
