3835c9b93dd4190c6190d0a2625a6d7b36d887c5
flygalaxies <iantolmay@mweb.co.za>
2017-09-07 10:28:44 +0200

    changed sound

--- Sound_lib/include/SoundEngine.hpp
+++ Sound_lib/include/SoundEngine.hpp
@@ -8,11 +8,35 @@
 
 class Sound {
 public:
+    
+    struct RIFF_Header {
+    char chunkID[4];
+    long chunkSize;//size not including chunkSize or chunkID
+    char format[4];
+    };
+    
+    struct WAVE_Format {
+    char subChunkID[4];
+    long subChunkSize;
+    short audioFormat;
+    short numChannels;
+    long sampleRate;
+    long byteRate;
+    short blockAlign;
+    short bitsPerSample;
+    };
+    
+    struct WAVE_Data {
+    char subChunkID[4]; //should contain the word data
+    long subChunk2Size; //Stores the size of the data block
+    };
+    
     Sound();
     Sound(const char *File);
     ~Sound();
 
     void initialize(const char* File);
+    bool loadWavFile(std::string filename, ALuint* buffer, ALsizei* size, ALsizei* frequency, ALenum* format);
     void play(int sleep, bool loop);
 
 private:
@@ -20,6 +44,9 @@ private:
     ALCcontext* _context;
     ALuint      _buffer;
     ALuint       _source;
+    ALint       _state;
+    ALsizei     _frequency;
+    ALenum      _format;
     bool        _loop = true;
     pthread_t   _thread[5];
 
