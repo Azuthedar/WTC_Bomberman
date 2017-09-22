c10445959d46ac0803b3b121b368a7171c36e67a
Claris RICHTER <antricht@e4r24p5.wethinkcode.co.za>
2017-09-22 07:00:23 +0200

    Added pull assets into install.sh. Just needs to be uncommented

--- install.sh
+++ install.sh
@@ -18,5 +18,10 @@ echo "Installing Python 3"
 brew install python3
 echo "Extracting NanoGUI"
 unzip ./Libs/nanogui.zip -d ./Libs
+#echo "Retrieving Assets"
+#git clone https://github.com/NodziGames/Bomberman_Assets.git BA
+#mv -f BA/Assets .
+#mv -f BA/Libs .
+#rm -rf BA
 cmake .
 make
--- pull_assets.sh
+++
@@ -1,5 +0,0 @@
-echo "Retrieving Assets"
-git clone https://github.com/NodziGames/Bomberman_Assets.git BA
-mv -f BA/Assets .
-mv -f BA/Libs .
-rm -rf BA
