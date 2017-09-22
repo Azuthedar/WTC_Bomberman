724402440246600f70930febb7199677fb25c313
Johann BUYS <jbuys@e4r23p5.wethinkcode.co.za>
2017-09-22 13:38:47 +0200

    Updated the shiz

--- install.sh
+++ install.sh
@@ -16,12 +16,13 @@ echo "Installing SDL2_MIXER..."
 brew install sdl2_mixer
 echo "Installing Python 3"
 brew install python3
+
+echo "Retrieving Assets"
+git clone https://github.com/Azuthedar/assets.git BA
+mv -f BA/Assets .
+mv -f BA/Libs .
+rm -rf BA
 echo "Extracting NanoGUI"
 unzip ./Libs/nanogui.zip -d ./Libs
-#echo "Retrieving Assets"
-#git clone https://github.com/NodziGames/Bomberman_Assets.git BA
-#mv -f BA/Assets .
-#mv -f BA/Libs .
-#rm -rf BA
 cmake .
 make
